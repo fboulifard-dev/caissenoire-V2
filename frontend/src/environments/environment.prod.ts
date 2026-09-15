80950162c1c9342104154fe77fa817f1e1fbaaf6
Florent Boulifard <florent.boulifard@outlook.fr>
2026-09-15 15:22:40 +0200

    mise à jour de la conf de prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.example.com',
+  apiUrl: 'https://api-asptt.fboulifard.com',
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT.firebaseapp.com',
-    projectId: 'YOUR_PROJECT',
-    storageBucket: 'YOUR_PROJECT.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID'
+    apiKey: "AIzaSyDlNZyo28iaalI2ZoqigR1eeMPpT2i5zyY",
+  authDomain: "caissenoire-dev-84854.firebaseapp.com",
+  projectId: "caissenoire-dev-84854",
+  storageBucket: "caissenoire-dev-84854.firebasestorage.app",
+  messagingSenderId: "786805881122",
+  appId: "1:786805881122:web:914088a4b9c3fece12cd7b",
+  measurementId: "G-9BQNRTY4LP"
   }
 };
